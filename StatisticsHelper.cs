d92cf85513e6ba997fda1fb09601a5a23fe896d4
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-12 22:09:19 +0100

    Empty array check added

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -3,6 +3,13 @@ public static class StatisticsHelper
 
     public static double CalculateAverage(int[] values)
     {
+
+        if (values == null || values.Length == 0)
+        {
+            Console.WriteLine("!Empty array!");
+            return 0;
+        }
+
         int sum = 0;
 
         foreach (var value in values) {
