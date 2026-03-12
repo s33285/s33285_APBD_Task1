ce6b33f3c85929c23f5fba00728f4bfa95f609af
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-12 21:58:59 +0100

    Added error handling

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@
 
         Console.WriteLine("Enter numbers(use space to separate): ");
         string input = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+
         string[] parts = input.Split(' ');
         int[] num = Array.ConvertAll(parts, int.Parse);
         double avg = StatisticsHelper.CalculateAverage(num);
