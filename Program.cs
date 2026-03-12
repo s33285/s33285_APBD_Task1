403ade6fcc52f8536df81497045912f2a977c029
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-12 21:56:40 +0100

    Added Input for numbers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Programc
+{
+    static void Main() {
+
+        Console.WriteLine("Enter numbers(use space to separate): ");
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+        int[] num = Array.ConvertAll(parts, int.Parse);
+        double avg = StatisticsHelper.CalculateAverage(num);
+        Console.WriteLine("Average: " + avg);
+    }
+
+}
