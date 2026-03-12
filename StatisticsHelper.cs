e2f8d3a2dc1d87d1609f48abedb7808a2a0d0b20
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-12 21:51:41 +0100

    Added average calculation method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,4 +1,14 @@
 public static class StatisticsHelper
 {
 
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+
+        foreach (var value in values) {
+            sum += value;
+        }
+
+        return (double)sum / values.Length;
+    }
 }
