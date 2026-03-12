2f318bb8085cc197c5fcfce32c0e0e9304e3fd4e
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-12 22:35:41 +0100

    Added CalculateMin

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -30,4 +30,15 @@ public static class StatisticsHelper
         return max;
     
     }
+
+    public static int CalculateMin(int[] values)
+    { 
+        if (values == null || values.Length ==0) return 0;
+
+        int min = values[0];
+        foreach (var v in values) { 
+            if (v < min) min = v;
+        }
+        return min;
+    }
 }
